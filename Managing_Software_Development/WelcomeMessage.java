27f9a1c522ad3ca138a2b37b5fd3125e84dc3900
Kulana Avinash <kulanaavinash2018@gmail.com>
2023-11-27 14:20:30 +1100

    Main project initialized

---
+++ Managing_Software_Development/WelcomeMessage.java
@@ -0,0 +1,9 @@
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Managing Software Development");
+        System.out.println("Our Team members are as follows:");
+        System.out.println("---------------------------------------------------");
+        System.out.println("KULANA AVINASH - 24354684");
+        System.out.println("____________________________________________");
+    }
+}
