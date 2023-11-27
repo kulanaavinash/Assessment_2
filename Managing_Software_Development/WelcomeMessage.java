82dd6553ccf8451b85561468b85e0c00c47bc68f
Kulana Avinash <kulanaavinash2018@gmail.com>
2023-11-27 14:28:42 +1100

    Update UI View

--- Managing_Software_Development/WelcomeMessage.java
+++ Managing_Software_Development/WelcomeMessage.java
@@ -1,9 +1,11 @@
 public class WelcomeMessage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Managing Software Development");
+        System.out.println("Welcome to Managing Software Development ");
+         System.out.println("PROG6001");
         System.out.println("Our Team members are as follows:");
         System.out.println("---------------------------------------------------");
-        System.out.println("KULANA AVINASH - 24354684");
+        System.out.println("KULANA AVINASH");
         System.out.println("____________________________________________");
     }
 }
+
